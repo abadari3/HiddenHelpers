f3018ca15c6c0797b818916c0df90c5b14103f7f
abadari3 <abadari3@gatech.edu>
2020-04-12 22:31:56 -0400

    undo

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ class Todo(db.Model):
     content = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Integer, default=0)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
-# t
     def __repr__(self):
         return '<Task %r>' %self.id
 
